1980ea4a7e3fd475b423e7482555825cc77dd693
siro844 <140879194+siro844@users.noreply.github.com>
2023-10-20 22:11:17 +0530

    working on global state management

--- csi_library_app-master/lib/cart.dart
+++ csi_library_app-master/lib/cart.dart
@@ -1,3 +1,5 @@
+import 'package:csi_library/globalvariables.dart';
+import 'package:csi_library/widgets/apptext.dart';
 import 'package:flutter/material.dart';
 
 class Cart extends StatelessWidget {
@@ -5,6 +7,61 @@ class Cart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+      body: Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: 175,
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(42, 74, 193, 1), 
+          
+          ),
+         child: Row(
+                 children: [
+                   IconButton(
+                    onPressed: (){
+                        Navigator.pop(context);
+                    }
+                   , icon: const Icon(Icons.arrow_back_ios,color: Colors.white,)),
+                   Container(
+                    margin: EdgeInsets.only(left: 100),
+                    child:   AppText(text: 'Menu'),
+                   ),
+                  
+                  
+                 ],
+               ),
+      ),
+      Expanded(
+        child: ListView.builder(
+          itemCount: cart.length,
+          itemBuilder: (context,index){
+                final CartItem = cart[index];
+                return (
+                  ListTile(
+                    leading: CircleAvatar(
+                      radius: 45,
+                      backgroundImage: NetworkImage(CartItem['image'].toString()),
+                    ),
+                    trailing: IconButton(onPressed: (){}, 
+                    icon: const Icon(Icons.delete,color: Colors.red,),
+                    ),
+                    title: Text(
+                      CartItem['title'].toString(),
+                      style: const TextStyle(fontWeight: FontWeight.bold,fontSize: 24),
+                    ),
+                    subtitle: Text(
+                      CartItem['genre'].toString(),
+                    ),
+                  )
+                );
+          }
+          ),
+        ),
+      ],
+    ),
+    );
   }
 }
---
+++ csi_library_app-master/lib/globalvariables.dart
@@ -0,0 +1,9 @@
+final cart=[
+  //image,genre,author,text;
+  {
+    'image':  'https://mir-s3-cdn-cf.behance.net/project_modules/1400/9d22a671392953.5bc4494680bce.jpg',
+    'genre':  'Art',
+    'author': 'Gesalt' ,
+    'title':   'This is Art',
+  }
+];
